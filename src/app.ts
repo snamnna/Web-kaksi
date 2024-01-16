c07b2b14de661a03a58d788de51e5266531426b5
Ilkka Kylmäniemi <ilkka.kylmaniemi@metropolia.fi>
2024-01-16 15:46:35 +0200

    interfacet tyypeiksi

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 require('dotenv').config();
-import express, {Request, Response} from 'express';
+import express from 'express';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import cors from 'cors';
