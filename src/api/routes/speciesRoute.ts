6edb9416ac831aeace1b28498d40cf34191feb95
sanna <sanna.lohkovuori@hotmail.com>
2024-01-19 16:44:57 +0200

    Species router tehty

--- src/api/routes/speciesRoute.ts
+++ src/api/routes/speciesRoute.ts
@@ -1,1 +1,16 @@
-// speciesRoute.ts
+import express from 'express';
+import {
+  speciesDelete,
+  speciesGet,
+  speciesListGet,
+  speciesPost,
+  speciesPut,
+} from '../controllers/speciesController';
+
+const router = express.Router();
+
+router.route('/').get(speciesListGet).post(speciesPost);
+
+router.route('/:id').get(speciesGet).put(speciesPut).delete(speciesDelete);
+
+export default router;
