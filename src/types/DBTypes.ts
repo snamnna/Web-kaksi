f37716adc43eb03b7c2c87b55b24649379b43c73
sanna <sanna.lohkovuori@hotmail.com>
2024-01-19 10:13:32 +0200

    The first push

--- src/types/DBTypes.ts
+++ src/types/DBTypes.ts
@@ -1,7 +1,12 @@
 type Category = {
   category_id: number;
   category_name: string;
-}
+};
+type Animal = {
+  animal_id: number;
+  animal_name: string;
+  species: number;
+  birthdate: Date;
+};
 
-
-export {Category};
+export {Category, Animal};
